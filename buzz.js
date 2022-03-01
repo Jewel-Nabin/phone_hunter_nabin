b550d858526da30f5685a34a35b0770714459048
Mohammed Jewel <jewelnabin@gmail.com>
2022-03-02 02:14:27 +0600

    Others part added

--- buzz.js
+++ buzz.js
@@ -14,7 +14,7 @@ const phonesDisplay = (phones) => {
         const div = document.createElement('div');
         div.classList.add("col-lg-4");
         div.innerHTML = 
-            `<div class="card border p-5 mb-5">
+            `<div class="card border p-5 mb-5 d-flex justify-content-center">
                     <img src="${data.image}" class="card-img-top h-100 w-25" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${data.phone_name}</h5>
@@ -24,7 +24,7 @@ const phonesDisplay = (phones) => {
                     </div>
                 </div>
             </div>
-            `
+            ` 
         parent.appendChild(div);
         // console.log(data);
     };
@@ -47,8 +47,16 @@ const setDetails = (info) => {
         `<div class="card border p-5 mb-5 container">
                     <img class="img-thumbnail w-50 h-100" src="${info.data.image}" class="card-img-top h-50 w-75" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${info.data.name}</h5>
-                    <p class="card-text">${info.data.releaseDate}</p>
+                    <p class="card-title"><span class="h6 pe-5">Name:</span>${info.data.name}</p>
+                    <p class="card-text"><span class="h6 pe-5">Brand:</span>${info.data.brand}</p>
+                    <p class="card-text"><span class="h6 pe-5">Id:</span>${info.data.slug}</p>
+                    <p class="card-text"><span class="h6 pe-5">Launch:</span>${info.data.releaseDate}</p>
+                    <p class="card-text"><span class="h6 pe-5">Chip:</span>${info.data.mainFeatures.chipSet}</p>
+                    <p class="card-text"><span class="h6 pe-5">Display:</span>${info.data.mainFeatures.displaySize}</p>
+                    <p class="card-text"><span class="h6 pe-5">Memory:</span>${info.data.mainFeatures.memory}</p>
+                    <p class="card-text"><span class="h6 pe-5">Storage:</span>${info.data.mainFeatures.storage}</p>
+                    <p class="card-text"><span class="h6 pe-5">Sensors:</span>${info.data.mainFeatures.sensors}</p>
+                    <p class="card-text"><span class="h6 pe-5">Others:</span>${info.data.others.WLAN} ${info.data.others.Bluetooth} ${info.data.others.GPS} ${info.data.others.NFC} ${info.data.others.Radio} ${info.data.others.USB}</p>
                 </div>
             </div>
         `;
